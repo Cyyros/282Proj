77ae4880c5b8f5239b4e5d01d4efd0127f7725e6
Cyyros <cyrosff@gmail.com>
2026-04-22 16:31:43 -0400

    Respawn and Placeholder Replacement

---
+++ Assets/Scripts/WaterRespawn.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaterRespawn : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.position;
+        }
+    }
+}
